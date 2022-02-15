4f9f5ddf38024dbefaaa81d4ff909fcd9521ff9d
gpspelle <gpsunicamp016@gmail.com>
2022-02-15 12:34:24 -0300

    missing value property on currency input

--- src/components/ProductForm/ProductForm.js
+++ src/components/ProductForm/ProductForm.js
@@ -433,6 +433,7 @@ export default function ProductForm() {
             allowNegativeValue={false}
             prefix="R$ "
             disableGroupSeparators
+            value={price}
           />
         </Form.Group>
         <ProductSellTypes
